3e9b7d6676c766cb54cbbd54755ab9f01331eff1
Camille Suder <camillesuder@gmail.com>
2022-12-01 11:48:44 -0800

    commit tree 07bf0ee3f4c1f986073f1b8c9a80b82222e0896c

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift();
+}
+
+function appendCat() {
+    var newArray = cats.slice();
+    newArray.push("Broom");
+    return newArray;
+}
+
+function prependCat(){
+    var newArray = cats.slice();
+    newArray.unshift("Arnold");
+    return newArray;
+}
+
+function removeLastCat(){
+    var newArray = cats.slice();
+    newArray.pop();
+    return newArray;
+}
+
+function removeFirstCat(){
+    var newArray = cats.slice();
+    newArray.shift();
+    return newArray;
+}
